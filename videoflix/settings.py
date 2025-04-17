3c70b4e51441b1fb4fd5c1b1ca8ad1bfc76cd2b5
KasZaim <122385481+KasZaim@users.noreply.github.com>
2025-04-17 16:27:25 +0200

    settings.px

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mfenw4-30*8*!5r!z@0wbwt(ahip*f%=i4^2ys4)6nb7c$yyl!'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
